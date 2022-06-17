ffd2b937ae0a5d1b64edf0a6cf5b005cf18ccfa9
Antony Rain <ragozin01@gmail.com>
2022-06-17 11:48:29 +0300

    Initial commit

---
+++ api_key.py
@@ -0,0 +1,6 @@
+"""
+Whale Alert uses API keys for access to the API.
+To create an API key please sign up here: https://whale-alert.io/.
+"""
+
+api_key = ""
---
+++ main.py
@@ -0,0 +1,24 @@
+from api_key import api_key
+import requests
+import time
+import pandas as pd
+
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 99
+
+local_date_time = time.localtime()  # get struct_time
+time_string = time.strftime("%H:%M:%S", local_date_time)
+print(time_string)
+
+timestamp = int(time.time()) - 900  # current Unix timestamp minus time in seconds for retrieving transactions
+value = "500000"  # minimum USD value of transactions returned
+currency_code = "eth"
+
+transactions = requests.get(
+    f'https://api.whale-alert.io/v1/transactions?api_key={api_key}&min_value={value}&start={timestamp}&currency={currency_code}')
+
+data = transactions.json()
+df = pd.DataFrame(data['transactions'])
+
+df.to_excel(f"{currency_code}_{time_string}.xlsx")
+# df.to_csv(f"{currency_code}_{time_string}.csv")
